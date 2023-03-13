d10829d9085fbabddd341021f64042879845bd6b
Julian Power <josejulianpower@gmail.com>
2023-03-13 17:37:44 -0600

    changing apps name to app, it's a folder

--- alembic/env.py
+++ alembic/env.py
@@ -2,8 +2,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from apps.models import Base 
-from apps.config import settings
+from app.models import Base 
+from app.config import settings
 from alembic import context
 
 # this is the Alembic Config object, which provides
